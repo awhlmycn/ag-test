6388361a9949c79d13f00183aa470382133d6bce
awhlmycn <mingyue.li@ebcapital.com.cn>
2023-11-07 18:15:06 +0800

    1. 保存基金项目的样式信息

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="container">
-        <div class="company_block">
+        <!-- <div class="company_block">
             <h2 class="title">1.  Usmile</h2>
             <div class="content">
                 <div class="c_block">
-                    <div class="c_title">1)公司概述</div>
+                    <div class="c_title">1) 公司概述</div>
                     <table class="c_base_table">
                         <tr>
                             <td>公司简介</td>
@@ -42,6 +42,153 @@
                         </tr>
                     </table>
                 </div>
+                <div class="c_block">
+                    <div class="c_title">2) 主要事项</div>
+                    <table class="c_base_table">
+                        <tr>
+                            <td>业务情况</td>
+                            <td>
+                                <p style="line-height: 150%;">
+                                    电动牙刷保持淘系渠道市场份额第一，持续增强品牌影响力，同时抖音渠道亦保持较好增长；公司23年重点发力海外市场；市场大盘增长缓慢，头部品牌集中度明显提升
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>上市进展</td>
+                            <td>暂无计划</td>
+                        </tr>
+                        <tr>
+                            <td>退出情况</td>
+                            <td>
+                                <p style="line-height: 150%;">
+                                    已实现金额2,100万，均为分红收益（其中钟鼎五号1,932万，五号青蓝168万）
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="c_block">
+                    <div class="c_title">3) 财务信息</div>
+                    <table class="c_base_table f_table">
+                        <tr>
+                            <td>人民币 (百万元) </td>
+                            <td>2022上半年</td>
+                            <td>2023年上半年</td>
+                            <td>同比</td>
+                        </tr>
+                        <tr>
+                            <td>营业收入</td>
+                            <td>804</td>
+                            <td>911</td>
+                            <td>+13%</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="c_block">
+                    <div class="c_title">3) 财务信息</div>
+                    <div class="c_title" style="font-weight: normal;">公司正在IPO敏感期暂不披露财务情况</div>
+                </div>
+            </div>
+        </div> -->
+        <div class="company_block">
+            <h2 class="title">1.  宁波鼎施</h2>
+            <div class="content">
+                <div class="c_block">
+                    <div class="c_title">1) 基金概况</div>
+                    <table class="c_base_table">
+                        <tr>
+                            <td>成立目的</td>
+                            <td>
+                                <p style="line-height: 150%;">
+                                    宁波鼎施、深圳鼎琨、苏州鼎珩三家为我司VC二期基金的三个投资主体，主要投向早期科技类企业及消费零售类企业
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>基金规模</td>
+                            <td>1.84亿</td>
+                        </tr>
+                        <tr>
+                            <td>成立时间</td>
+                            <td>2019年5月6号</td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="c_block">
+                    <div class="c_title">2) 运营情况</div>
+                    <table class="c_base_table">
+                        <tr>
+                            <td>出资人</td>
+                            <td>
+                                <p style="line-height: 150%;">
+                                    GP上海鼎函，认缴出资100万元；南京鼎沁，认缴出资1.8亿元，已实缴100%（按项目call款）
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>投资情况</td>
+                            <td>
+                                <p style="line-height: 150%;">
+                                    截止2023年6月30日，宁波鼎施已投37个项目，已投资项目合计1.8亿元，基金回报倍数为1.9倍
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="c_block">
+                    <div class="c_title">3) 主要目的</div>
+                    <table class="c_base_table f_table fund_table">
+                       <tr>
+                            <td>序号</td>
+                            <td>项目名称</td>
+                            <td>投资时间(首笔)</td>
+                            <td>总价值(万元)</td>
+                            <td>股比</td>
+                            <td>总倍数</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>魔筷</td>
+                            <td>2019年5月</td>
+                            <td>8,750</td>
+                            <td>2.87%</td>
+                            <td>4.8</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>赢彻科技</td>
+                            <td>2019年5月</td>
+                            <td>8,750</td>
+                            <td>2.87%</td>
+                            <td>4.4</td>
+                        </tr>
+        
+                        <tr>
+                            <td>3</td>
+                            <td>冻品云</td>
+                            <td>2019年5月</td>
+                            <td>8,750</td>
+                            <td>2.87%</td>
+                            <td>1.3</td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>批批网</td>
+                            <td>2019年5月</td>
+                            <td>8,750</td>
+                            <td>2.87%</td>
+                            <td>1.3</td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>天行云</td>
+                            <td>2019年5月</td>
+                            <td>8,750</td>
+                            <td>2.87%</td>
+                            <td>1.8</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
@@ -62,50 +209,74 @@ export default {
 
 <style>
     .container{
-        width: 800px;
+        width: 900px;
         font-family: "华文楷体", "STKaiti", "SimKai", "KaiTi", cursive !important;
     }
-    .company_block .title{
+    .title{
         font-size: 14pt;
         font-family: "华文楷体", "STKaiti", "SimKai", "KaiTi", cursive;
+    }
+    .c_block{
+        margin: 14px 0;
     }
     .c_title{
         font-size: 11pt;
         font-family: "华文楷体", "STKaiti", "SimKai", "KaiTi", cursive;
         font-weight: bold;
-        margin-bottom: 8px;
+        margin-bottom: 14px;
     }
-
     table {
         border-collapse: collapse;
-        width: 87.3%;
+        width: 95%;
+        font-family: "华文楷体", "STKaiti", "SimKai", "KaiTi", cursive;
+        font-size: 10.5pt;
     }
-    .c_base_table th, .c_base_table td {
-        border: 2px dotted #000;
+    .c_base_table td {
+        border: 1px dotted #000;
         padding: 8px;
+        padding-top: 4px;
         text-align: left;
         vertical-align: top; /* 设置垂直对齐到顶部 */
     }
-
     /* 第一列 */
     .c_base_table td:first-child {
         background-color: #d9e2f3; /* 设置背景颜色 */
         font-weight: bold; /* 设置字体加粗 */
         width: 20%;
-        border-left: 2px dashed #c8c8c8;
-        border-right: 2px solid #000;
+        border-left: 1px dashed #c8c8c8;
         box-sizing: border-box;
     }
-    td p {
+    .c_base_table td p {
        margin-top:0px;
+       margin-bottom:4px;
     }
     /* 设置第一行的上边框样式 */
-    .c_base_table tr:first-child th, .c_base_table tr:first-child td {
-        border-top: 2px solid #4473c5; /* 设置上边框样式 */
+    .c_base_table tr:first-child td {
+        border-top: 1px solid #4473c5; /* 设置上边框样式 */
     }
-
     /* 设置最后一行的下边框样式 */
-    .c_base_table tr:last-child th, .c_base_table tr:last-child td {
-        border-bottom: 2px solid #4473c5; /* 设置下边框样式 */
+    .c_base_table tr:last-child td {
+        border-bottom: 1px solid #4473c5; /* 设置下边框样式 */
+    }
+    /* 财务的表格信息 */
+    .f_table td{
+        text-align: center;
+        vertical-align: top;
+    }
+    /* 财务第一列 */
+    .f_table td:first-child{
+        background-color:#fff;
+        text-align: left;
+        font-weight: bold;
+        width: 25%;
+    }
+    /* 第一行 */
+    .f_table tr:first-child td{
+        background-color: #d9e2f3; /* 设置背景颜色 */
+        font-weight: bold;
+    }
+    /* 基金第一列 */
+    .fund_table td:first-child{
+        width: 10%;
     }
 </style>
